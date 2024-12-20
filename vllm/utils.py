9b0aab585ab0c314178339f9ea8e7eb82352c049
youkaichao <youkaichao@gmail.com>
2024-12-20 15:30:13 -0800

    add world size
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- vllm/utils.py
+++ vllm/utils.py
@@ -1752,7 +1752,7 @@ def memory_profiling(
     from vllm.distributed.parallel_state import get_world_group
     rank = get_world_group().rank_in_group
     world_size = get_world_group().world_size
-    filename = f"memory_snapshot_rank_{rank}_of{world_size}.pickle"
+    filename = f"memory_snapshot_rank_{rank}_of_{world_size}.pickle"
     torch.cuda.memory._dump_snapshot(filename)
     print(f"Memory snapshot saved to {filename}")
 
